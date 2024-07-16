dadc84deae4bfdc9d7aae62d2543b957a09b3252
Libo3 <valentinlibonati33@gmail.com>
2024-07-16 11:20:29 -0300

    Añadido componente NavigationBar a Layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
-import NavigationBar from "@/components/navigation/NavigationBar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      <NavigationBar />
+      <div className="h-screen">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
+        ratione veritatis natus laboriosam qui recusandae corporis impedit
+        provident perferendis voluptatem magni vel voluptatibus nesciunt
+        deserunt sequi. Explicabo sequi tempore aliquam!
+      </div>
+      <div className="h-screen">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
+        ratione veritatis natus laboriosam qui recusandae corporis impedit
+        provident perferendis voluptatem magni vel voluptatibus nesciunt
+        deserunt sequi. Explicabo sequi tempore aliquam!
+      </div>
+      <div className="h-screen">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
+        ratione veritatis natus laboriosam qui recusandae corporis impedit
+        provident perferendis voluptatem magni vel voluptatibus nesciunt
+        deserunt sequi. Explicabo sequi tempore aliquam!
+      </div>
     </main>
   );
 }
