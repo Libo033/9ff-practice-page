a137c69ac01fea7baae799d21e41c9499432e4ad
Libo3 <valentinlibonati33@gmail.com>
2024-07-22 19:33:44 -0300

    En proceso NewsCard y News en Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import ContactCard from "@/components/cards/ContactCard";
+import NewsCard from "@/components/cards/NewsCard";
 import HomeFlyer from "@/components/home/HomeFlyer";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="pt-[91px] max-w-screen-2xl m-auto">
       <HomeFlyer />
+      <section className="pt-10 mx-4">
+        <p className="text-4xl font-extrabold underline decoration-[#b0a17b] text-black">News</p>
+        <NewsCard />
+      </section>
       <ContactCard />
     </main>
   );
---
+++ src/components/cards/NewsCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// IDEA CARD QUE EN CLICK CAMBIE DE IMAGEN Y APAREZCA EL TEXTO DE INFORMACION.
+// O CARD CON IMAGEN QUE CAMBIE EN HOVER CON TODA LA INFO YA EN SU CONTENIDO.
+// PENSAR
+
+const NewsCard = () => {
+  return (
+    <article>NewsCard</article>
+  )
+}
+
+export default NewsCard
