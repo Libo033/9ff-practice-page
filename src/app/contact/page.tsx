d7cfe941059dc3e5224588b0ebf43d51e8e8aa52
Libo3 <valentinlibonati33@gmail.com>
2024-07-25 21:18:40 -0300

    Creado ContactInfo

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,12 @@
 import ContactForm from "@/components/Form/ContactForm";
+import ContactInfo from "@/components/others/ContactInfo";
 import React from "react";
 
 const Contact = () => {
   return (
     <main className="pt-[91px] max-w-screen-2xl m-auto">
       <ContactForm />
+      <ContactInfo />
     </main>
   );
 };
---
+++ src/components/others/ContactInfo.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+const ContactInfo = () => {
+  return (
+    <div className="bg-white border-2 mb-8 mx-4 sm:relative sm:mx-20 md:mx-32 lg:mx-64 xl:mx-[27%] 2xl:mx-[30%]">
+      <div className="flex flex-col p-4 gap-6 sm:flex-row sm:justify-around">
+        <div className="sm:w-1/2">
+          <p className="text-2xl text-[#b0a17b] font-bold uppercase underline decoration-black sm:text-xl sm:text-right">
+            address
+          </p>
+          <p className="sm:text-sm sm:text-right">9FF engineering GmbH</p>
+          <p className="sm:text-sm sm:text-right">Rüdigerstr. 3rd</p>
+          <p className="sm:text-sm sm:text-right">44319 Dortmund</p>
+          <p className="sm:text-sm sm:text-right">Germany</p>
+        </div>
+        <div className="sm:w-1/2">
+          <p className="text-2xl text-[#b0a17b] font-bold uppercase underline decoration-black sm:text-xl">
+            email
+          </p>
+          <p className="sm:text-sm">Service requests</p>
+          <p className="sm:text-sm">email@9ff.com</p>
+          <p className="sm:text-sm">General inquiries</p>
+          <p className="sm:text-sm">email@9ff.com</p>
+          <p className="sm:text-sm">Press inquiries</p>
+          <p className="sm:text-sm">email@9ff.com</p>
+        </div>
+      </div>
+      <div className="flex flex-col p-4 gap-6 sm:flex-row sm:justify-around">
+        <div className="sm:w-1/2">
+          <p className="text-2xl text-[#b0a17b] font-bold uppercase underline decoration-black sm:text-xl sm:text-right">
+            business times
+          </p>
+          <p className="sm:text-sm sm:text-right">
+            Monday to Thursday 8 a.m. - 5 p.m.
+          </p>
+          <p className="sm:text-sm sm:text-right">Fridays 8 a.m. - 3 p.m.</p>
+          <p className="sm:text-sm sm:text-right">Saturday by appointment</p>
+          <p className="sm:text-sm sm:text-right">Closed on Sunday</p>
+        </div>
+        <div className="sm:w-1/2">
+          <p className="text-2xl text-[#b0a17b] font-bold uppercase underline decoration-black sm:text-xl">
+            telephone
+          </p>
+          <p className="sm:text-sm">+99 99 99 99 9999</p>
+        </div>
+      </div>
+      <span className="sm:absolute sm:border border-[#b0a17b50] sm:w-[94%] sm:bottom-[50%] sm:translate-y-3 sm:mx-[3%]"></span>
+      <span className="sm:absolute sm:border border-[#b0a17b50] sm:h-[90%] sm:right-[50%] sm:top-0 sm:translate-y-[5%]"></span>
+    </div>
+  );
+};
+
+export default ContactInfo;
