f6a6cbeb73dd1060e20e06ab23523a4007e026c2
Libo3 <valentinlibonati33@gmail.com>
2024-07-29 11:05:34 -0300

    Creado componente Team9ff para About9ff page

--- src/app/about-9ff/page.tsx
+++ src/app/about-9ff/page.tsx
@@ -3,12 +3,14 @@ import ContactCard from "@/components/cards/ContactCard";
 import AboutHeader from "@/components/others/AboutHeader";
 import OurMission from "@/components/cards/OurMission";
 import Image from "next/image";
+import Team9ff from "@/components/others/Team9ff";
 
 const About9ff = () => {
   return (
     <main className="pt-[91px] max-w-screen-2xl m-auto">
       <section className="flex flex-col gap-6">
         <AboutHeader />
+        <Team9ff />
         <div className="px-4 h-40 rounded-lg md:px-8 md:h-52 lg:px-32 lg:h-80 2xl:h-96">
           <Image
             className="h-full flex justify-center rounded-lg object-cover"
---
+++ src/components/others/Team9ff.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Team9ff = () => {
+  return (
+    <div className="px-4 md:px-8 lg:px-32">
+      <div className="bg-white rounded-lg shadow-md p-6 pb-8 w-full flex flex-col">
+        <p className="w-full text-center pb-2 text-2xl underline decoration-[#b0a17b] text-black">
+          THE 9FF TEAM
+        </p>
+        <p className="font-light text-sm text-center pb-6 sm:text-base text-[#b0a17b]">
+          'Exceptional performance can only be achieved with extraordinary
+          people'
+        </p>
+        <p className="text-pretty text-justify">
+          At the top of 9FF is the "Brain": <i>Jan Fatthauer</i>. He perfected
+          his engineering knowledge in the field of vehicle technology so much
+          that today he is one of the few worldwide, if not the only "engine
+          artist", which can get well over 1,000 hp from a production engine.
+          <br />
+          <br />
+          His young team at 9FF consists of highly motivated engineers, talented
+          body builders, gifted cars. Masters, hardworking fellows and
+          apprentices. And they all live the same dream: do everything a little
+          better.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Team9ff;
