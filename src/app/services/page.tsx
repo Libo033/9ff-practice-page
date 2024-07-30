149315ffca7052a4052d0945998cb9d417c0319d
Libo3 <valentinlibonati33@gmail.com>
2024-07-30 11:01:51 -0300

    Creado titulo de services page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -4,7 +4,11 @@ import ContactCard from "@/components/cards/ContactCard";
 const Services = () => {
   return (
     <main className="pt-[91px] max-w-screen-2xl m-auto">
-      Services
+      <div className="px-4 md:px-8 lg:px-32">
+        <p className="bg-white rounded-lg shadow-lg mt-4 py-4 text-xl text-center font-semibold border-4 border-[#b0a17b]">
+          SERVICES FOR OUR CUSTOMERS
+        </p>
+      </div>
       <ContactCard />
     </main>
   );
